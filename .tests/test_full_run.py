9278f8ba623d95c85d915289b86672521fb29889
ctbus <ctbushman@gmail.com>
2022-03-15 12:18:52 -0400

    Add test coassembly output

--- .tests/test_full_run.py
+++ .tests/test_full_run.py
@@ -18,84 +18,87 @@ class FullRunTests(unittest.TestCase):
 
         self.samples_fp = os.path.join(self.temp_dir, "samples.csv")
         self.samples_content = """
-            TEST0,{r0r1},{r0r2}\n
-            TEST1,{r1r1},{r1r2}\n
-            TEST2,{r2r1},{r2r2}\n
-            TEST3,{r3r1},{r3r2}\n
-            TEST4,{r4r1},{r4r2}\n
-            """.format(
-                r0r1 = os.path.join(self.temp_dir, "data/TEST0_R1.fastq.gz"),
-                r0r2 = os.path.join(self.temp_dir, "data/TEST0_R2.fastq.gz"),
-                r1r1 = os.path.join(self.temp_dir, "data/TEST1_R1.fastq.gz"),
-                r1r2 = os.path.join(self.temp_dir, "data/TEST1_R2.fastq.gz"),
-                r2r1 = os.path.join(self.temp_dir, "data/TEST2_R1.fastq.gz"),
-                r2r2 = os.path.join(self.temp_dir, "data/TEST2_R2.fastq.gz"),
-                r3r1 = os.path.join(self.temp_dir, "data/TEST3_R1.fastq.gz"),
-                r3r2 = os.path.join(self.temp_dir, "data/TEST3_R2.fastq.gz"),
-                r4r1 = os.path.join(self.temp_dir, "data/TEST4_R1.fastq.gz"),
-                r4r2 = os.path.join(self.temp_dir, "data/TEST4_R2.fastq.gz"))
+TEST0,{r0r1},{r0r2}\n
+TEST1,{r1r1},{r1r2}\n
+TEST2,{r2r1},{r2r2}\n
+TEST3,{r3r1},{r3r2}\n
+TEST4,{r4r1},{r4r2}\n
+""".format(
+                r0r1 = os.path.join(self.data_fp, "reads/TEST0_R1.fastq.gz"),
+                r0r2 = os.path.join(self.data_fp, "reads/TEST0_R2.fastq.gz"),
+                r1r1 = os.path.join(self.data_fp, "reads/TEST1_R1.fastq.gz"),
+                r1r2 = os.path.join(self.data_fp, "reads/TEST1_R2.fastq.gz"),
+                r2r1 = os.path.join(self.data_fp, "reads/TEST2_R1.fastq.gz"),
+                r2r2 = os.path.join(self.data_fp, "reads/TEST2_R2.fastq.gz"),
+                r3r1 = os.path.join(self.data_fp, "reads/TEST3_R1.fastq.gz"),
+                r3r2 = os.path.join(self.data_fp, "reads/TEST3_R2.fastq.gz"),
+                r4r1 = os.path.join(self.data_fp, "reads/TEST4_R1.fastq.gz"),
+                r4r2 = os.path.join(self.data_fp, "reads/TEST4_R2.fastq.gz"))
         with open(self.samples_fp, "w") as f:
             f.write(self.samples_content)
         
         self.config_fp = os.path.join(self.temp_dir, "sunbeam_config.yml")
         self.config_content = """
             all:\n
-            \troot: {root}\n
-            \toutput_fp: sunbeam_output\n
-            \tsamplelist_fp: samples.csv\n
-            \tpaired_end: true\n
-            \tdownload_reads: false\n
-            \tversion: 2.1.1+dev0.g43432e1.d20220208\n
+              root: {root}\n
+              output_fp: sunbeam_output\n
+              samplelist_fp: samples.csv\n
+              paired_end: true\n
+              download_reads: false\n
+              version: 2.1.1+dev0.g43432e1.d20220208\n
             qc:\n
-            \tsuffix: qc\n
-            \tthreads: 4\n
-            \tjava_heapsize: 512M\n
-            \tleading: 3\n
-            \ttrailing: 3\n
-            \tslidingwindow: [4, 15]\n
-            \tminlen: 36\n
-            \tadapter_fp: \n
-            \tfwd_adapters: [GTTTCCCAGTCACGATC, GTTTCCCAGTCACGATCNNNNNNNNNGTTTCCCAGTCACGATC]\n
-            \trev_adapters: [GTTTCCCAGTCACGATC, GTTTCCCAGTCACGATCNNNNNNNNNGTTTCCCAGTCACGATC]\n
-            \tkz_threshold: 0.55\n
-            \tpct_id: 0.5\n
-            \tfrac: 0.6\n
-            \thost_fp: ''\n
+              suffix: qc\n
+              threads: 4\n
+              java_heapsize: 512M\n
+              leading: 3\n
+              trailing: 3\n
+              slidingwindow: [4, 15]\n
+              minlen: 36\n
+              adapter_fp: ''\n
+              fwd_adapters: [GTTTCCCAGTCACGATC, GTTTCCCAGTCACGATCNNNNNNNNNGTTTCCCAGTCACGATC]\n
+              rev_adapters: [GTTTCCCAGTCACGATC, GTTTCCCAGTCACGATCNNNNNNNNNGTTTCCCAGTCACGATC]\n
+              kz_threshold: 0.55\n
+              pct_id: 0.5\n
+              frac: 0.6\n
+              host_fp: ''\n
             classify:\n
-            \tsuffix: classify\n
-            \tthreads: 4\n
-            \tkraken_db_fp: ''\n
+              suffix: classify\n
+              threads: 4\n
+              kraken_db_fp: ''\n
             assembly:\n
-            \tsuffix: assembly\n
-            \tmin_length: 300\n
-            \tthreads: 4\n
+              suffix: assembly\n
+              min_length: 300\n
+              threads: 4\n
             annotation:\n
-            \tsuffix: annotation\n
-            \tmin_contig_len: 500\n
-            \tcircular_kmin: 10\n
-            \tcircular_kmax: 1000\n
-            \tcircular_min_len: 3500\n
+              suffix: annotation\n
+              min_contig_len: 500\n
+              circular_kmin: 10\n
+              circular_kmax: 1000\n
+              circular_min_len: 3500\n
             blast:\n
-            \tthreads: 4\n
-            \tblastdbs:\n
-            \troot_fp: ''\n
+              threads: 4\n
+            blastdbs:\n
+              root_fp: ''\n
             mapping:\n
-            \tsuffix: mapping\n
-            \tgenomes_fp: {ref}\n
-            \tsamtools_opts: ''\n
-            \tthreads: 4\n
+              suffix: mapping\n
+              genomes_fp: {ref}\n
+              samtools_opts: ''\n
+              threads: 4\n
             download:\n
-            \tsuffix: download\n
-            \tthreads: 4\n
-            \tsbx_coassembly:\n
-            \tthreads: 4\n
-            \tgroup_file: ''\n
+              suffix: download\n
+              threads: 4\n
+              sbx_coassembly: ''\n
+              threads: 4\n
+              group_file: ''\n
+            sbx_coassembly:\n
+              threads: 4\n
+              group_file: ''\n
             sbx_demic:\n
-            \tthreads: 4 #--thread_num\n
-            \tkeepall: FALSE #--output_all\n
-            \textras: "-W 701 -D 50 -L 31" # Other parameters passed to DEMIC.pl\n
+              threads: 4 #--thread_num\n
+              keepall: FALSE #--output_all\n
+              extras: "-W 701 -D 50 -L 31" # Other parameters passed to DEMIC.pl\n
             """.format(root = os.path.join(self.temp_dir),
-                ref = os.path.join(self.data_fp, "data/reference/akk-genome.fasta"))
+                ref = os.path.join(self.data_fp, "reference/akk-genome.fasta"))
         with open(self.config_fp, "w") as f:
             f.write(self.config_content)
 
@@ -116,7 +119,6 @@ class FullRunTests(unittest.TestCase):
             self.config_fp,
             "--use-conda",
             "all_demic",
-            "--"
             "--directory",
             self.temp_dir,
         ])
@@ -126,4 +128,4 @@ class FullRunTests(unittest.TestCase):
         with open(self.all_ptr_fp) as f:
             self.assertEqual(next(f), "\tTEST0\tTEST1\tTEST2\tTEST3\tTEST4")
             for val in next(f).split("\t")[1:]:
-                self.assertEqual(int(val), 10)
+                self.assertEqual(int(val), 3)
