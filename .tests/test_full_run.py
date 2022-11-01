35e4f7dc3a40adabfecbda620ea58437884e8211
ctbus <ctbushman@gmail.com>
2022-11-01 14:14:37 -0400

    A bunch of stuff

--- .tests/test_full_run.py
+++ .tests/test_full_run.py
@@ -8,8 +8,7 @@ class FullRunTests(unittest.TestCase):
     def setUp(self) -> None:
         self.temp_dir = tempfile.mkdtemp()
 
-        self.data_fp = os.path.join(self.temp_dir, "data/")
-        shutil.copytree(".tests/data/", self.data_fp)
+        self.reads_fp = ".tests/reads/"
 
         self.project_dir = os.path.join(self.temp_dir, "project/")
 
@@ -17,14 +16,14 @@ class FullRunTests(unittest.TestCase):
             "sunbeam",
             "init",
             "--data_fp",
-            os.path.join(self.data_fp, "reads/"),
+            self.reads_fp,
             self.project_dir
         ])
 
         self.config_fp = os.path.join(self.project_dir, "sunbeam_config.yml")
 
         self.output_fp = os.path.join(self.project_dir, "sunbeam_output")
-        #shutil.copytree(".tests/data/sunbeam_output", self.output_fp)
+        shutil.copytree(".tests/sunbeam_output", self.output_fp)
 
         self.all_ptr_fp = os.path.join(self.output_fp, "mapping/demic/DEMIC_OUT/all_PTR.txt")
     
@@ -38,14 +37,17 @@ class FullRunTests(unittest.TestCase):
             "run",
              "--configfile", 
             self.config_fp,
-            "all_demic",
+            "run_demic",
             "--directory",
             self.temp_dir,
+            "-n",
         ])
 
         # Check output
         self.assertTrue(os.path.exists(self.all_ptr_fp))
         with open(self.all_ptr_fp) as f:
-            self.assertEqual(next(f), "\tTEST0\tTEST1\tTEST2\tTEST3\tTEST4\n")
+            self.assertEqual(next(f), "\tSample1\tSample2\tSample3\n")
             for val in next(f).split("\t")[1:]:
-                self.assertEqual(round(float(val)), 3)
+                self.assertEqual(round(float(val)), 2)
+            for val in next(f).split("\t")[1:]:
+                self.assertEqual(round(float(val)), 2)
