c517387a2465f1137b24d048b8e5ae3d0fb1d0e5
ctbus <ctbushman@gmail.com>
2022-12-06 12:03:25 -0500

    Remove extraneous files, add install_demic_R.R

--- envs/install.R
+++
@@ -1,9 +0,0 @@
-if("lme4" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("lme4", repos="https://cloud.r-project.org")
-}
-if("factominer" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("factominer", repos="https://cloud.r-project.org")
-}
-if("reshape2" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("reshape2", repos="https://cloud.r-project.org")
-}
