f0f2b38d751de286459e2a1910b179b1eb844f3e
ctbus <ctbushman@gmail.com>
2022-12-19 22:07:35 -0500

    New testing data

--- .tests/e2e/test_full_run.py
+++ .tests/e2e/test_full_run.py
@@ -1,58 +1,88 @@
+import csv
 import os
-import subprocess as sp
+import pytest
 import shutil
-import unittest
+import subprocess as sp
 import tempfile
 
 
-class FullRunTests(unittest.TestCase):
-    def setUp(self) -> None:
-        self.temp_dir = tempfile.mkdtemp()
+@pytest.fixture
+def setup():
+    temp_dir = tempfile.mkdtemp()
 
-        self.data_fp = os.path.join(self.temp_dir, "data/")
-        shutil.copytree(".tests/data/", self.data_fp)
+    reads_fp = os.path.abspath(".tests/data/reads/")
 
-        self.project_dir = os.path.join(self.temp_dir, "project/")
+    project_dir = os.path.join(temp_dir, "project/")
 
-        sp.check_output(
-            [
-                "sunbeam",
-                "init",
-                "--data_fp",
-                os.path.join(self.data_fp, "reads/"),
-                self.project_dir,
-            ]
-        )
+    sp.check_output(["sunbeam", "init", "--data_fp", reads_fp, project_dir])
 
-        self.config_fp = os.path.join(self.project_dir, "sunbeam_config.yml")
+    config_fp = os.path.join(project_dir, "sunbeam_config.yml")
 
-        self.output_fp = os.path.join(self.project_dir, "sunbeam_output")
-        shutil.copytree(".tests/data/sunbeam_output", self.output_fp)
+    config_str = f"sbx_demic: {{extras: '-W 701 -D 50 -L 31'}}"
 
-        self.all_ptr_fp = os.path.join(
-            self.output_fp, "mapping/demic/DEMIC_OUT/all_PTR.txt"
-        )
+    sp.check_output(
+        [
+            "sunbeam",
+            "config",
+            "modify",
+            "-i",
+            "-s",
+            f"{config_str}",
+            f"{config_fp}",
+        ]
+    )
 
-    def tearDown(self):
-        shutil.rmtree(self.temp_dir)
+    yield temp_dir, project_dir
 
-    def test_full_run(self):
-        # Run the test job.
-        sp.check_output(
-            [
-                "sunbeam",
-                "run",
-                "--configfile",
-                self.config_fp,
-                "all_demic",
-                "--directory",
-                self.temp_dir,
-            ]
-        )
+    shutil.rmtree(temp_dir)
 
-        # Check output
-        self.assertTrue(os.path.exists(self.all_ptr_fp))
-        with open(self.all_ptr_fp) as f:
-            self.assertEqual(next(f), "\tTEST0\tTEST1\tTEST2\tTEST3\tTEST4\n")
-            for val in next(f).split("\t")[1:]:
-                self.assertEqual(round(float(val)), 3)
+
+@pytest.fixture
+def run_sunbeam(setup):
+    temp_dir, project_dir = setup
+
+    # Run the test job.
+    sp.check_output(
+        [
+            "sunbeam",
+            "run",
+            "--profile",
+            project_dir,
+            "all_demic",
+            "--directory",
+            temp_dir,
+        ]
+    )
+
+    output_fp = os.path.join(project_dir, "sunbeam_output")
+
+    all_ptr_fp = os.path.join(
+        output_fp, "mapping/demic/DEMIC_OUT/all_PTR.txt"
+    )
+
+    benchmarks_fp = os.path.join(project_dir, "stats/")
+
+    yield all_ptr_fp, benchmarks_fp
+
+
+def test_full_run(run_sunbeam):
+    all_ptr_fp, benchmarks_fp = run_sunbeam
+
+    # Check output
+    assert os.path.exists(all_ptr_fp)
+    with open(all_ptr_fp) as f:
+        assert next(f) == "\tTEST0\tTEST1\tTEST2\tTEST3\tTEST4\n"
+        for val in next(f).split("\t")[1:]:
+            assert round(float(val)) == 3
+
+
+def test_benchmarks(run_sunbeam):
+    all_ptr_fp, benchmarks_fp = run_sunbeam
+
+    filename = os.listdir(benchmarks_fp)[0]
+    with open(os.path.join(benchmarks_fp, filename)) as f:
+        rd = csv.DictReader(f, delimiter="\t")
+        for r in rd:
+            assert (
+                float(r["cpu_time"]) < 0.5
+            ), f"cpu_time for {r['rule']} is higher than 0.5: {r['cpu_time']}"
