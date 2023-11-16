1c36f4d75a20485f8d516d8b8511170e50aa13be
ctbus <ctbushman@gmail.com>
2023-11-16 17:16:03 -0500

    Update run_demic.R

--- scripts/run_demic.R
+++ scripts/run_demic.R
@@ -1,9 +1,8 @@
 files <- list.files(path=snakemake@input[['input']], pattern="*.cov3", full.names=TRUE, recursive=FALSE)
 lapply(files, function(x) {
-    X <- read.csv(x, header = FALSE, stringsAsFactors = TRUE)
-    colnames(X) <- c("log_cov", "GC_content", "sample", "contig", "length")
+    X <- read.csv(x, stringsAsFactors = TRUE)
     O <- demic::est_ptr(X)
     
-    dir.create(snakemake@output[['out']])
+    #dir.create(snakemake@output[['out']])
     write.table(O, paste(snakemake@output[['out']], "/", tools::file_path_sans_ext(basename(x)), ".ptr", sep=""), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
 })
